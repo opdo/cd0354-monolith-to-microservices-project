588de3596318a84557b4192746c891012c94de3c
Vinh Pham <vinhphamdhsp@live.com>
2022-11-14 11:15:33 +0700

    Microservice

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export POSTGRES_USERNAME=vinh
+export POSTGRES_PASSWORD=Vinh2981995
+export POSTGRES_HOST=vinhproject3.cnzjqjlih9cp.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=vinhproject3
+export AWS_BUCKET=arn:aws:s3:::project3-vinh-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=vinh-dev
 export URL=http://localhost:8100
---
+++ udagram-api-user/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': 'vinh',
+  'password': 'Vinh2981995',
+  'database': 'vinhproject3',
+  'host': 'vinhproject3.cnzjqjlih9cp.us-east-1.rds.amazonaws.com',
+  'dialect': 'postgres',
+  'aws_region': 'us-east-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 'project3-vinh-dev',
+  'url': 'http://localhost:8100',
+  'jwt': {
+    'secret': 'vinh-dev',
+  },
+};
